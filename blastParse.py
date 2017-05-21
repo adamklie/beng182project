9a6450dfffa604db3bd9c41e2f36c32599f33cc8
Angela Frentzen <angela@Angelas-MacBook-Air-7.local>
2017-05-21 14:11:28 -0700

    Adds the universal Script

---
+++ blastParse.py
@@ -0,0 +1,34 @@
+#######################################################
+# To Run this program you MUST have the following files:
+# 'fastaSeq.fasta' - Original, large fasta file
+# 'readFasta.py' - Trims out 101 protein sequences
+# 'uniprot-all.fasta' - Uniprot Database File
+########################################################
+import os
+
+#Original fasta protein sequence
+originalFasta = 'fastaSeq.fasta'
+
+#Make a trimmed version of the original fasta file called trimmedProteins.fasta
+print ''
+print 'Trimming protein sequence \n' 
+os.system('python readFasta.py' )
+protSeq = 'trimmedProteins.fasta'
+
+#installs Blast
+os.system('brew install blast')
+
+#Blasting swissprot db against the trimmed 101 protein sequence through swissprot db
+print ''
+print 'Preparing Blast Database\n'
+uniprotDb = 'uniprot-all.fasta' 
+#preps the blast search by making a blast db
+makeBlast  = 'makeblastdb -in ' +  uniprotDb + ' -dbtype prot'
+os.system(makeBlast)
+
+#Querys the blast db to pull out hits, stored in blastOut
+print ''
+print 'Running Blast Queries'
+blastOut = 'blastOut.txt'
+queryBlast = 'blastp -query ' + protSeq + ' -db ' + uniprotDb + ' > ' + blastOut
+os.system(queryBlast)
